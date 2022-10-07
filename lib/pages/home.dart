a90b416dae2f7bcd06b73f8977361d68c47f87c4
pajeeh <l256647@dac.unicamp.br>
2022-10-07 20:20:30 -0300

    update

---
+++ lib/pages/home.dart
@@ -0,0 +1,5 @@
+// Página inicial, deve ser a primeira a ser carregada
+// e deve ser a única a ser carregada quando o usuário
+// clicar no botão de home do dispositivo.
+// A pagina deve exibir uma lista em forma de grade, de todos os
+// recursos cadastrados no aplicativo.
